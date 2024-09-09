5772c76086e4a4263a80c47611b6dc7b09626105
DevikaAcharya <devikaacharya861@gmail.com>
2024-09-09 12:03:54 +0530

    This is a boring class!

---
+++ hello.java
@@ -0,0 +1,9 @@
+import  java.util.*;
+imort java.util.Scanner;
+
+public class hello{
+public  static void main(String[]args){
+	System.out.println("Hello This is a training program for DEVOPS!!");
+	
+}
+}
